7fe726899e1d6c89c807bf840a9bcff9a5b2f84a
Lucas Vieira <lucasvieiradasilva07@gmail.com>
2021-05-29 16:24:13 -0300

    Repository creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			jokes: [],
+			quantity: 1,
+			getJoke: () => {
+				return fetch('https://api.chucknorris.io/jokes/random')
+							.then(res => res.json())
+							.then(joke => { 
+								let newJokes = this.state.jokes;
+								newJokes.push(joke.value);
+								this.setState({ jokes: newJokes })
+							});
+			}
+		}
+	}
+
+	//componentDidMount() {
+		//this.state.getJoke();
+	//}
+
+	onQuantityChange = (event) => {
+		this.setState({ quantity: event.target.value })
+		this.state.getJoke();
+	}
+
+	render () {
+		const { jokes } = this.state;
+
+		const jokesArray = jokes.map((joke, i) => {
+			return (
+				<p key={i}>
+					{joke}
+				</p>
+			);
+		});
+
+		//todo 
+		//make it so that the input works properly when
+		//lowering the quantity
+		return (
+			<div className="App">
+				<h1>Chuck Norris joke generator</h1>
+				<p>Number of jokes:</p>
+				<input 
+					type="number" 
+					min="1" 
+					max="5" 
+					onChange={this.onQuantityChange}
+				/>
+				{jokesArray}
+			</div>
+		);
+	}
 }
 
 export default App;
